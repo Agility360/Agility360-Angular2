642e55bd44c29e6b5b4724eb720533da3014a222
Lawrence McDaniel <lpm0073@gmail.com>
2018-03-05 09:06:04 -0600

    added angular router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { TosComponent }      from './tos/tos.component';
+
+const routes: Routes = [
+  { path: 'tos', component: TosComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './/app-routing.module';
+import { TosComponent } from './tos/tos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavbarComponent
+    AppNavbarComponent,
+    TosComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
